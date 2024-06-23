3078bb91183213cb31ffa62b10375524f6f9daba
David Elliott <david@starjump.org>
2024-06-23 20:30:11 +0100

    Split files and twak config

---
+++ lua/options.lua
@@ -0,0 +1,34 @@
+-------------
+-- options --
+--------------
+
+vim.opt.number = true		
+vim.opt.numberwidth = 2
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true	
+vim.opt.splitright = true
+
+vim.opt.signcolumn = "no"
+vim.opt.cursorline = true
+
+vim.opt.wrap = false
+
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+
+vim.opt.virtualedit = "block"
+
+vim.opt.inccommand = "split"
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+
