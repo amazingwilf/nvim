4df4c16dd33c9b97ac96ae4c6395f7c7f6feea47
David Elliott <david@starjump.org>
2024-07-10 17:15:14 +0100

    Added noice

--- lua/david/lazy/comment.lua
+++ lua/david/lazy/comment.lua
@@ -1,19 +1,19 @@
 return {
-  "numToStr/Comment.nvim",
-  event = { "BufReadPre", "BufNewFile" },
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  config = function()
-    -- import comment plugin safely
-    local comment = require("Comment")
-
-    local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-    -- enable comment
-    comment.setup({
-      -- for commenting tsx, jsx, svelte, html files
-      pre_hook = ts_context_commentstring.create_pre_hook(),
-    })
-  end,
+    "folke/noice.nvim",
+        event = "VeryLazy",
+        opts = {
+        routes = {
+            {
+            filter = { event = "notify", find = "No information available" },
+            opts = { skip = true },
+            },
+        },
+        presets = {
+            lsp_doc_border = true,
+          },
+        },
+        dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+        },
 }
